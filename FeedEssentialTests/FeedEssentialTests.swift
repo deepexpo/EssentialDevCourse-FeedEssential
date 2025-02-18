4313e001e61e69440ad8d7cbcc403ae884351b45
Kuldeep SIngh <kuldeep.expo@gmail.com>
2025-02-18 10:45:56 -0600

    Initial Commit

---
+++ FeedEssentialTests/FeedEssentialTests.swift
@@ -0,0 +1,17 @@
+//
+//  FeedEssentialTests.swift
+//  FeedEssentialTests
+//
+//  Created by Kuldeep Singh on 18/02/25.
+//
+
+import Testing
+@testable import FeedEssential
+
+struct FeedEssentialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
